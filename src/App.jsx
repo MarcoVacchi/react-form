d9bc7ad91ca91a8691ff34ff2f65e0f136faf85e
MarcoVacchi <mvacchi816@gmail.com>
2025-04-09 15:31:53 +0200

    added:  delete button

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Article from './data/Article'
 
 function App() {
 
-  const [firstArray, setFirstArray] = useState({ Article });
+  const [firstArray, setFirstArray] = useState(["Sviluppatore web", "Designer grafico", "Copywriter", "Project manager", "Data analyst"]);
   const [newArray, setNewArray] = useState("");
 
   // console.log(firstArray.Article[0])
@@ -11,20 +11,29 @@ function App() {
     event.preventDefault();
     const arrayNew = [...firstArray, newArray];
     setFirstArray(arrayNew);
-    console.log(setFirstArray(arrayNew))
+  }
+  const deleteLi = (indexToDelete) => {
+    const myNewArray = firstArray.filter((element, index) => indexToDelete !== index);
+    setFirstArray(myNewArray);
   }
 
   return (
     <>
-      <form onSubmit={addNewArray}>
-        <input type="text" onChange={event => setNewArray(event.target.value)} />
+      <div className='container justify-content-center mt-5'>
+        <h1 className='text-black'>ARTICLE LIST</h1>
         <ul className="list-group container mt-2">
           {firstArray.map((element, index) =>
-            <li key={index} className="list-group-item disabled mt-2 text-white bg-primary">
-              {element}
+            <li key={index} className="disabled mt-2 text-white bg-primary">
+              <h3>{element}</h3>
+              <button onClick={() => deleteLi(index)}>Delete article</button>
             </li>)
           }
         </ul>
+      </div>
+      <form className='container' onSubmit={addNewArray}>
+        <div className='container d-flex justify-content-center mt-5'>
+          <input className='text-center' type="text" placeholder='add new article' onChange={event => setNewArray(event.target.value)} />
+        </div>
       </form >
     </>
 
@@ -34,9 +43,7 @@ function App() {
 export default App
 
 /*
-
-Milestone 2
-Aggiungiamo in pagina un semplice form con un campo input in cui inserire il titolo di un nuovo articolo del blog.
-Al submit del form, mostrare la lista degli articoli aggiornati.
+1.  Aggiungere la possibilità di cancellare ciascun articolo utilizzando un'icona.
+ <button onClick={() => deleteLi(index)}>Delete article</button>
 
 */
