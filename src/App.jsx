88283accb890efac48cfb419d08ee1ce43800f13
MarcoVacchi <mvacchi816@gmail.com>
2025-04-09 14:18:38 +0200

    feat: exercise complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,29 @@ import Article from './data/Article'
 
 function App() {
 
+  const [firstArray, setFirstArray] = useState({ Article });
+  const [newArray, setNewArray] = useState("");
+
+  // console.log(firstArray.Article[0])
+  const addNewArray = event => {
+    event.preventDefault();
+    const arrayNew = [...firstArray, newArray];
+    setFirstArray(arrayNew);
+    console.log(setFirstArray(arrayNew))
+  }
+
   return (
     <>
-      <ul class="list-group container mt-2">
-        {Article.map(element =>
-          <li class="list-group-item disabled mt-2 text-white bg-primary">
-            {element}
-          </li>)
-        }
-      </ul>
+      <form onSubmit={addNewArray}>
+        <input type="text" onChange={event => setNewArray(event.target.value)} />
+        <ul className="list-group container mt-2">
+          {firstArray.map((element, index) =>
+            <li key={index} className="list-group-item disabled mt-2 text-white bg-primary">
+              {element}
+            </li>)
+          }
+        </ul>
+      </form >
     </>
 
   )
@@ -19,5 +33,10 @@ function App() {
 
 export default App
 
+/*
 
-// Creare una pagina che visualizzi una lista di articoli, mostrandone solo il titolo.
+Milestone 2
+Aggiungiamo in pagina un semplice form con un campo input in cui inserire il titolo di un nuovo articolo del blog.
+Al submit del form, mostrare la lista degli articoli aggiornati.
+
+*/
