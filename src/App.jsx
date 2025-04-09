2300bf3a0373d2a1ffa9ab67a9cbc406f9983330
MarcoVacchi <mvacchi816@gmail.com>
2025-04-09 15:48:09 +0200

    feat: added icon

--- src/App.jsx
+++ src/App.jsx
@@ -15,27 +15,31 @@ function App() {
   const deleteLi = (indexToDelete) => {
     const myNewArray = firstArray.filter((element, index) => indexToDelete !== index);
     setFirstArray(myNewArray);
+
   }
 
   return (
     <>
-      <div className='container justify-content-center mt-5'>
-        <h1 className='text-black'>ARTICLE LIST</h1>
-        <ul className="list-group container mt-2">
-          {firstArray.map((element, index) =>
-            <li key={index} className="disabled mt-2 text-white bg-primary">
-              <h3>{element}</h3>
-              <button onClick={() => deleteLi(index)}>Delete article</button>
-            </li>)
-          }
-        </ul>
-      </div>
-      <form className='container' onSubmit={addNewArray}>
-        <div className='container d-flex justify-content-center mt-5'>
-          <input className='text-center' type="text" placeholder='add new article' onChange={event => setNewArray(event.target.value)} />
+      <main className='bg-secondary-subtle'>
+        <div className='container justify-content-center mt-5'>
+          <h1 className='text-black'>ARTICLE LIST</h1>
+          <ul className="list-group container mt-2">
+            {firstArray.map((element, index) =>
+              <li key={index} className="disabled mt-2 text-white bg-primary p-2">
+                <h3>{element}</h3>
+                <i className="fa-solid fa-trash text-black p-3 mr-2" onClick={() => deleteLi(index)}><span className='p-2'>Delete article</span></i>
+              </li>)
+            }
+          </ul>
         </div>
-      </form >
+        <form className='container' onSubmit={addNewArray}>
+          <div className='container d-flex justify-content-center mt-5'>
+            <input className='text-center' type="text" placeholder='add new article' onChange={event => setNewArray(event.target.value)} />
+          </div>
+        </form >
+      </main>
     </>
+
 
   )
 }
